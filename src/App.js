64f82cb4a340b176540bd8f02dc0405cd28cf355
almeida1 <engsoftware1@gmail.com>
2023-09-04 18:32:37 -0300

    Função - consultar catalogo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from "./Home";
+import "./styles.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ConsultaCatalogo from "./componentes/ConsultaCatalogo";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/produtos/catalogo" element={<ConsultaCatalogo />} />
+      </Routes>
+    </Router>
   );
-}
-
+};
 export default App;
---
+++ src/componentes/ConsultaCatalogo/index.js
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+const ConsultaCatalogo = () => {
+  const [produtos, setProdutos] = useState([]);
+  const [erro, setErro] = useState(null);
+  useEffect(() => {
+    const consulta = async () => {
+      try {
+        const resposta = await fetch("http://localhost:8080/api/v1/produtos");
+        if (!resposta.ok) {
+          throw new Error();
+        }
+        const dados = await resposta.json(); //retorna um array de objetos json
+        //apresenta os dados na console como um string json
+        console.log(JSON.stringify(dados));
+        //carrega os dados na variavel produto
+        setProdutos(dados);
+      } catch (error) {
+        console.log(error.message);
+        setErro(error.message);
+      }
+    };
+    consulta();
+  }, []);
+  if (erro)
+    return (
+      <>
+        <h3>Consulta Catalogo </h3>
+        <p>Erro na consulta: {erro}</p>
+      </>
+    );
+  return (
+    <div>
+      <h3>Consulta Catalogo </h3>
+      <table id="produtos">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Descrição</th>
+            <th>Custo</th>
+            <th>Quant</th>
+          </tr>
+        </thead>
+        <tbody>
+          {produtos.map((produto) => (
+            <tr key={produto.id}>
+              <td>{produto.id}</td>
+              <td>{produto.descricao}</td>
+              <td>{produto.custo}</td>
+              <td>{produto.quantidadeNoEstoque}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+};
+export default ConsultaCatalogo;
